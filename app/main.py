a4c1163d4ec33063fb289e122833e056024c0a3c
Santosh <santosh@Santoshs-MacBook-Air.local>
2022-07-02 23:07:07 +0530

    Commit - 1

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Path, Depends
 from fastapi.responses import JSONResponse
+from app.data.items import item_data
+from app.data.store import store_data
 
 app = FastAPI()
 
@@ -14,5 +16,16 @@ def loginUser(loginData: dict):
     else:
         if (hash(loginData['pass'])==userLoginDetails[loginData['uname']]):
             return JSONResponse(status_code=200, content={'success': True})
+        else:
+            return JSONResponse(status_code=400, content={'success': False})
 
-    
+
+@app.get("/get_store_details")
+def get_store_details():
+    return JSONResponse(status_code=200, content = {'success': True,
+    'data': store_data["Bangalore Outlet Details"]})
+
+@app.get("/get_item_details")
+def get_item_details():
+    return JSONResponse(status_code=200, content = {'success': True,
+    'data': item_data["Data"]})
