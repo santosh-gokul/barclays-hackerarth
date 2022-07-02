e33f0971475f3c70d7986245b3952b10df598270
Santosh <santosh@Santoshs-MacBook-Air.local>
2022-07-02 23:58:12 +0530

    Commit - 1

--- app/models/api.py
+++ app/models/api.py
@@ -4,13 +4,14 @@ from pydantic import BaseModel, validator
 
 class LoginData(BaseModel):
     uname: str = Body(...)
+    password: str = Body(...)
 
-    @validator(uname)
+    @validator('uname')
     def uname_validator(cls, v):
         assert len(v)>0, "Make sure the username has value"
         return v
 
-    # @validator(password)
-    # def password_validator(cls, v):
-    #     assert len(v)>0, "Make sure the password has value"
-    #     return v
+    @validator('password')
+    def password_validator(cls, v):
+        assert len(v)>0, "Make sure the password has value"
+        return v
